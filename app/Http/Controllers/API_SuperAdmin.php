6a32e1dd980a3415e44948d556b5fd85db8c7947
Andy Maulana Yusuf <goingtoprofandy@gmail.com>
2019-08-21 12:28:01 +0700

    Set Up Umum On Super Admin

--- app/Http/Controllers/API_SuperAdmin.php
+++ app/Http/Controllers/API_SuperAdmin.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Transformations\M_LevelUserTransformer;
+use App\Transformations\M_ProvinsiTransformer;
+use App\Transformations\M_KotaTransformer;
+use App\Transformations\M_KecamatanTransformer;
+use App\Transformations\M_KelurahanTransformer;
+use App\Transformations\M_KewargaNegaraanTransformer;
+use App\Transformations\M_StatusPerkawinanTransformer;
 use Illuminate\Http\Request;
 use App\M_LevelUser;
+use App\M_Provinsi;
+use App\M_Kota;
+use App\M_Kecamatan;
+use App\M_Kelurahan;
+use App\M_KewargaNegaraan;
+use App\M_StatusPerkawinan;
 use Auth;
 use Hash;
 
@@ -32,7 +44,6 @@ class API_SuperAdmin extends Controller {
             ->transformWith(new M_LevelUserTransformer)
             ->toArray(), 200);
     }
-
     // Delete Level User
     public function deleteLevelUser(Request $request, M_LevelUser $level) {
         // This is check the authentication is already or not
@@ -61,7 +72,6 @@ class API_SuperAdmin extends Controller {
             ->transformWith(new M_LevelUserTransformer)
             ->toArray(), 200);
     }
-
     // Update Level User
     public function updateLevelUser(Request $request, M_LevelUser $level) {
         // This is check the authentication is already or not
@@ -99,27 +109,81 @@ class API_SuperAdmin extends Controller {
     }
 
     // Create Provinsi
+    public function createProvinsi(Request $request, M_Provinsi $provinsi) {
+
+    }
     // Update Provinsi
+    public function updateProvinsi(Request $request, M_Provinsi $provinsi) {
+
+    }
     // Delete Provinsi
+    public function deleteProvinsi(Request $request, M_Provinsi $provinsi) {
+
+    }
     
     // Create Kota
+    public function createKota(Request $request, M_Kota $kota) {
+
+    }
     // Update Kota
+    public function updateKota(Request $request, M_Kota $kota) {
+
+    }
     // Delete Kota
+    public function deleteKota(Request $request, M_Kota $kota) {
+
+    }
 
     // Create Kecamatan
+    public function createKecamatan(Request $request, M_Kecamatan $kecamatan) {
+
+    }
     // Update Kecamatan
+    public function updateKecamatan(Request $request, M_Kecamatan $kecamatan) {
+        
+    }
     // Delete Kecamatan
+    public function deleteKecamatan(Request $request, M_Kecamatan $kecamatan) {
+        
+    }
 
     // Create Kelurahan
+    public function createKelurahan(Request $request, M_Kelurahan $kelurahan) {
+        
+    }
     // Update Kelurahan
+    public function updateKelurahan(Request $request, M_Kelurahan $kelurahan) {
+        
+    }
     // Delete Kelurahan
+    public function deleteKelurahan(Request $request, M_Kelurahan $kelurahan) {
+        
+    }
 
     // Create Kewarga Negaraan
+    public function createKewargaNegaraan(Request $request, M_KewargaNegaraan $kewarga_negaraan) {
+        
+    }
     // Update Kewarga Negaraan
+    public function updateKewargaNegaraan(Request $request, M_KewargaNegaraan $kewarga_negaraan) {
+        
+    }
     // Delete Kewarga Negaraan
+    public function deleteKewargaNegaraan(Request $request, M_KewargaNegaraan $kewarga_negaraan) {
+        
+    }
 
     // Create Status Kawin
+    public function createStatusKawin(Request $request, M_StatusPerkawinan $status_perkawinan) {
+        
+    }
     // Update Status Kawin
+    public function updateStatusKawin(Request $request, M_StatusPerkawinan $status_perkawinan) {
+        
+    }
     // Delete Status Kawin
+    public function deleteStatusKawin(Request $request, M_StatusPerkawinan $status_perkawinan) {
+        
+    }
 
 }
